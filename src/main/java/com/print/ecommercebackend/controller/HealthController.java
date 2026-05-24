4e76ed2783d2f03b433d279532a08c2f91421b4d
Aditya Ramakrishnan <a.ramakrishnan@wir-machen-druck.de>
2026-05-24 22:34:53 +0530

    feat:added basic controller and entity

--- src/main/java/com/print/ecommercebackend/controller/HealthController.java
+++ src/main/java/com/print/ecommercebackend/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.print.ecommercebackend;
+package com.print.ecommercebackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/print/ecommercebackend/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.print.ecommercebackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public List<String> getProducts() {
+
+        return List.of(
+                "Macbook Pro",
+                "iPhone",
+                "Gaming Mouse"
+        );
+    }
+}
