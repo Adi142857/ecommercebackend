3a8d89af52dbc8e77ee991eef0e0e2232f437c60
Aditya Ramakrishnan <a.ramakrishnan@wir-machen-druck.de>
2026-05-24 15:04:08 +0530

    project setup

---
+++ src/main/java/com/print/ecommercebackend/HealthController.java
@@ -0,0 +1,12 @@
+package com.print.ecommercebackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Backend is running healthy";   
+    }
+}
